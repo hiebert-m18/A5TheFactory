188ff7f837987b459429fd4715da76145ba031e3
malacaihiebert <malacaihiebert@gmail.com>
2023-04-06 10:40:05 -0500

    update

--- Factory.java
+++ Factory.java
@@ -22,7 +22,7 @@ public class Factory {
         // ***** objects *****
         ProgramInfo programinfo = new ProgramInfo();
         Random random = new Random();
-        ArrayList<Integer> shelf = new ArrayList<Integer>(5);
+        ArrayList<Integer> shelf = new ArrayList<Integer>();
         Lock lock = new ReentrantLock();
         
         Producer producer = new Producer();
@@ -35,86 +35,111 @@ public class Factory {
 
         // ***** Main Processing *****
         Runnable producerRun = () -> {
-            // lock the shelf
-            lock.lock();
-            try {
-                producer.stockShelf(shelf, random);
-            } catch (Exception e) {
-                System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
-            } finally {
-                lock.unlock();
-            }// end try finally
+            synchronized(producer) {
+                // lock the shelf
+                lock.lock();
+                try {
+                    producer.stockShelf(shelf, random);
+                } catch (Exception e) {
+                    System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
+                } finally {
+                    lock.unlock();
+                }// end try finally   
+            }
         };// end producerRun
         
         Runnable consumer1Run = () -> {
             double n = 0;
             double item = 0;
+            boolean access = true;
             
-            // lock the shelf
-            lock.lock();
-            try {
-                item = consumer1.takeItem(shelf);
-            } catch (Exception e) {
-                System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
-            } finally {
-                lock.unlock();
-            }// end try finally
-            
-            // print and compute
-            n = Math.pow(2, item);
-            System.out.println(Thread.currentThread().getName() + " took 1 item and calculated: 2^" + item + " = " + n + ". There are " + shelf.size() + " items left.");
-            try {
-                Thread.sleep(500);
+            synchronized(consumer1) {
+                try {
+                    // lock the shelf
+                    lock.lock();
+                    try {
+                        item = consumer1.takeItem(shelf);
+                    } catch (Exception e) {
+                        System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
+                        access = false;
+                    } finally {
+                        lock.unlock();
+                    }// end try finally
+                    
+                    // print and compute
+                    if (access == true) {
+                        n = Math.pow(2, item);
+                        System.out.println(Thread.currentThread().getName() + " took 1 item and calculated: 2^" + item + " = " + n + ". There are " + shelf.size() + " items left.");
+                    }// end if
+                    
+                    // sleep
+                    Thread.sleep(50);
+                }
+                catch (InterruptedException e) {}   
             }
-            catch (InterruptedException e) {}
         };// end consumer1Run
         
         Runnable consumer2Run = () -> {
             int a = 0;
             int b = 0;
+            boolean access = true;
             
-            // lock the shelf
-            lock.lock();
-            try {
-                a = consumer2.takeItem(shelf);
-                b = consumer2.takeItem(shelf);
-            } catch (Exception e) {
-                System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
-            } finally {
-                lock.unlock();
-            }// end try finally
-            
-            // print and compute
-            System.out.println(Thread.currentThread().getName() + " took 2 items and calculated: " + a + " + " + b + " = " + (a+b) + ". There are " + shelf.size() + " items left.");
-            try {
-                Thread.sleep(750);
+            synchronized(consumer2) {
+                try {
+                    // lock the shelf
+                    lock.lock();
+                    try {
+                        a = consumer2.takeItem(shelf);
+                        b = consumer2.takeItem(shelf);
+                    } catch (Exception e) {
+                        System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
+                        access = false;
+                    } finally {
+                        lock.unlock();
+                    }// end try finally
+                    
+                    // print and compute
+                    if (access == true) {
+                        System.out.println(Thread.currentThread().getName() + " took 2 items and calculated: " + a + " + " + b + " = " + (a+b) + ". There are " + shelf.size() + " items left.");
+                    }// end if
+                    
+                    // sleep
+                    Thread.sleep(75);
+                }
+                catch (InterruptedException e) {}   
             }
-            catch (InterruptedException e) {}
         };// end consumer2Run
         
         Runnable consumer3Run = () -> {
             int a = 0;
-            
-            // lock the shelf
-            lock.lock();
-            try {
-                a = consumer3.takeItem(shelf);
-            } catch (Exception e) {
-                System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
-            } finally {
-                lock.unlock();
-            }// end try finally
-            
-            // print and compute
-            System.out.println(Thread.currentThread().getName() + " took 1 items and it is: " + a + ". There are " + shelf.size() + " items left.");
-            try {
-                Thread.sleep(1000);
+            boolean access = true;
+            synchronized(consumer3) {
+                try {
+                    // lock the shelf
+                    lock.lock();
+                    try {
+                        a = consumer3.takeItem(shelf);
+                    } catch (Exception e) {
+                        System.out.println(Thread.currentThread().getName() + " couldn't access the shelf.");
+                        access = false;
+                    } finally {
+                        lock.unlock();
+                    }// end try finally
+                    
+                    // print and compute
+                    if (access == true) {
+                        System.out.println(Thread.currentThread().getName() + " took 1 items and it is: " + a + ". There are " + shelf.size() + " items left.");
+                    }// end if
+                    
+                    // sleep
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException e) {}   
             }
-            catch (InterruptedException e) {}
         };// end consumer3Run
         
         
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             Thread producerThread = new Thread(producerRun, "Producer");
             Thread consumer1Thread = new Thread(consumer1Run, "Consumer 1");
             Thread consumer2Thread = new Thread(consumer2Run, "Consumer 2");
